a7b0357562f844b91843db8bff5e8e1369dd90ca
mwafak <mwafaqarbash3@gmail.com>
2019-06-14 14:03:47 +0200

    deployment

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+const path = require("path");
 // connet Database
 connectDB();
 
 // Init Middleware
 
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("Api running"));
+
 // Define route
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (rea, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
