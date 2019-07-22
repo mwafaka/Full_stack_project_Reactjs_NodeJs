4e828fea8fde6857e83087a5f03b02d82a56150b
mwafaka <mwafaqarbash3@gmail.com>
2019-07-22 23:16:48 +0200

    card css fixed

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -19,59 +19,43 @@ const ProfileAbout = ({
         <div class="line" />
       </Fragment>
     )}
+    {/* <h1 class="text-light large">Offers</h1> */}
+    <div  >
     <h1 class="text-light large">Offers</h1>
-    <div class="">
-    <h1 class="text-light large">Offers</h1>
+<div style={{display:"flex"}}>
       {offers.map((offer, index) => (
-        <Row key={index} className="m-2">
-          <Col sm="12 " className=" text-light">
-            <div
-              body
-              inverse
-              style={{ backgroundColor: "#333", borderColor: "#333" }}
-            >
-              
-              <CardText
-                className="
-             blue text-primary"
-              >
-                {offer}
-              </CardText>
-              <Link to="/reservation" className="btn btn-primary" >
-              <i class="fas fa-bookmark"></i> Reservation
-      </Link>
-              {/* <Button style={{ fontSize: "25px", color: "white" }}>
-                reservation
-              </Button> */}
-            </div>
-          </Col>
-        </Row>
-      ))}
-    </div>
-   <h1 class="text-light large">Events</h1>
- <div >
-   {events.map((event,index)=>(
-//      <div className="ss">
-// <div className=" p-6  my-1  rounded">
-// <Toast key={index} className="bg-grey ">
-// <ToastHeader icon="primary" className="text-primary">
-// Event
-// </ToastHeader>
-// <div style={{fontSize:'1.5rem', fontFamily:"arial-black"}}>
-// {event}
-// </div>
-// </Toast>
-// </div>
-// </div>
-
-<div class="row">
-<div class="col s12 m7">
-  <div class="card">
-    <div class="card-image">
-      <img src={"../profile/../components/img/showcase.jpg"}/>
-      <span class="card-title"> {event}</span>
+      <div key={index} class="" style={{width:"33%",margin:"1%"}}>
+<div class="" >
+  <div class="card"  style={{width:"100%",padding:"0.2%" }}>
+    <div class="card-image1">
     </div>
     <div class="card-content">
+    <h2 class="card-title"> {offer}</h2>
+      <p>Description</p>
+    </div>
+    <div class="card-action">
+    <Link to="/reservation" className="btn btn-primary" >
+              <i class="fas fa-bookmark"></i> Reservation
+       </Link>
+    </div>
+  </div>
+</div>
+</div>
+      ))}
+    </div>
+    </div>
+   <h1 class="text-light large">Events</h1>
+ <div style={{display:"flex"}} >
+   {events.map((event,index)=>(
+<div key={index} class="" style={{width:"33%",margin:"1%"}}>
+<div class="" >
+  <div class="card"  style={{width:"100%",padding:"0.2%" }}>
+    <div class="card-image">
+      
+     
+    </div>
+    <div class="card-content">
+    <h2 class="card-title"> {event}</h2>
       <p>I am a very simple card. I am good at containing small bits of information.
       I am convenient because I require little markup to use effectively.</p>
     </div>
@@ -85,17 +69,34 @@ const ProfileAbout = ({
 </div>
 
 <h1 class=" large1">Offers Permanents</h1>
- <div >
+ <div  style={{display:"flex"}}>
    {permanents.map((permanent,index)=>(
-     <div className="ss">
-<div className=" p-6  my-1  rounded">
-<Toast key={index} className="bg-grey ">
-<ToastHeader icon="success" className="text-primary" style={{fontSize:"50%"}}>Permanents
-</ToastHeader>
-<div style={{fontSize:'1.5rem', fontFamily:"arial-black"}}>
-{permanent}
-</div>
-</Toast>
+//      <div className="ss">
+// <div className=" p-6  my-1  rounded">
+// <Toast key={index} className="bg-grey ">
+// <ToastHeader icon="success" className="text-primary" style={{fontSize:"50%"}}>Permanents
+// </ToastHeader>
+// <div style={{fontSize:'1.5rem', fontFamily:"arial-black"}}>
+// {permanent}
+// </div>
+// </Toast>
+// </div>
+// </div>
+<div key={index} class="" style={{width:"33%",margin:"1%"}}>
+<div class="" >
+  <div class="card"  style={{width:"100%",padding:"0.2%" }}>
+    <div class="">
+      
+     
+    </div>
+    <div class="card-content">
+    <h2 class="card-title"> {permanent}</h2>
+      <p></p>
+    </div>
+    <div class="card-action">
+      <a href="#">This is a link</a>
+    </div>
+  </div>
 </div>
 </div>
   ))}
