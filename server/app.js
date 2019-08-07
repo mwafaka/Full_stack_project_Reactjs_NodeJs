529e9e1714a3be9371a5ffd1b88780c036e0c6b1
Oliver Weber <oliver@mondough.io>
2019-08-07 11:40:31 +0200

    now doesn't swallow path

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-const path = require("path");
 const fileUpload = require("express-fileupload");
-// connet Database
+// connect Database
 connectDB();
 
 // Init Middleware
@@ -11,21 +10,12 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(fileUpload());
 
-console.log(process.env.NODE_ENV);
-
-app.use("/uploads", express.static(__dirname + "uploads"));
+app.use("/api/uploads", express.static(__dirname + "uploads"));
 
 // Define route
-app.use("/users", require("./routes/api/users"));
-app.use("/auth", require("./routes/api/auth"));
-app.use("/profile", require("./routes/api/profile"));
-app.use("/posts", require("./routes/api/posts"));
-
-//Set static folder
-
-app.use(express.static("client/build"));
-app.get("*", (rea, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 module.exports = app;
